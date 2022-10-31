6fe3f5777ec7dad790a9edcc7377655971f5c16f
Oleksii Timofeiev <oleksii.timofieiev@sleepnumber.com>
2022-10-31 09:45:08 +0200

    household bot mods

--- Go/src/HouseholdBot/main.go
+++ Go/src/HouseholdBot/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	// "fmt"
 	"log"
+	"strings"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -21,12 +23,43 @@ func processCommand(bot *tgbotapi.BotAPI, command string, update tgbotapi.Update
 			if _, err := bot.Send(msg); err != nil {
 				log.Panic(err)
 			}
+		} else if command == "not_available_today" {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID,
+				energyOutOfService[int(time.Now().Weekday())])
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		} else if command == "not_available_week" {
+			var sb strings.Builder
+
+			for i := 1; i < 7; i++ {
+				sb.WriteString(energyOutOfService[i])
+			}
+			sb.WriteString(energyOutOfService[0])
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID,
+				sb.String())
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
 		}
 	}
 }
 
+var energyOutOfService [7]string
+
 func main() {
 	bot, err := tgbotapi.NewBotAPI(botToken)
+
+	energyOutOfService[1] = "Понеділок: 6-10, 15-19\n"
+	energyOutOfService[2] = "Вівторок: 0-4, 9-13, 18-22\n"
+	energyOutOfService[3] = "Середа: 3-7, 12-16, 21-24\n"
+	energyOutOfService[4] = "Четвер: 0-1, 6-10, 15-19\n"
+	energyOutOfService[5] = "П'ятниця: 0-4, 9-13, 18-22\n"
+	energyOutOfService[6] = "Суббота: 3-7, 12-16, 21-24\n"
+	energyOutOfService[0] = "Неділя: 0-1, 6-10, 15-19"
 
 	if err != nil {
 		log.Panic(err)
@@ -34,9 +67,11 @@ func main() {
 
 	bot.Send(tgbotapi.NewSetMyCommands(
 		// tgbotapi.BotCommand{Command: "help", Description: "status => чи є електрика"},
-		tgbotapi.BotCommand{Command: "status", Description: "Перевірити наявність електоренергіЇ"}))
+		tgbotapi.BotCommand{Command: "status", Description: "Перевірити наявність електоренергіЇ"},
+		tgbotapi.BotCommand{Command: "not_available_today", Description: "План відключень на сьогодні"},
+		tgbotapi.BotCommand{Command: "not_available_week", Description: "План відключень на тиждень"}))
 
-	bot.Debug = true
+	bot.Debug = false
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
