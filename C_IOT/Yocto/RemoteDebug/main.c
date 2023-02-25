808a24b5ad97c0da2f2d0c58b620ce4cd71b29c1
Oleksii Timofeiev <oleksii.timofieiev@sleepnumber.com>
2023-02-25 12:34:52 +0200

    new

---
+++ C_IOT/Yocto/RemoteDebug/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	printf("Hello world!\n");
+	printf("Hello world!\n");
+	printf("Hello world!\n");
+memset((char *)0x0, 1, 100);
+    int test;
+
+    printf("%d", test);
+
+	return 0;
+}
