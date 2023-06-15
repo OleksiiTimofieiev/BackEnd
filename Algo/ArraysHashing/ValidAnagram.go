a895ba62fe2c4bd930f57aec3ef3cfb0b4a42387
Oleksii Timofieiev <contrbas@gmail.com>
2023-06-15 12:39:02 +0300

    new

--- Algo/ArraysHashing/ValidAnagram.go
+++ Algo/ArraysHashing/ValidAnagram.go
@@ -1,22 +1,27 @@
 package arrayshashing
 
+import "log"
+
 func IsAnagram(s string, t string) bool {
 	if len(s) != len(t) {
-        return false
-    }
-    
-    var freq [26]int
-    
-    for idx := 0; idx < len(s); idx++ {
-        freq[s[idx] - 'a']++
-        freq[t[idx] - 'a']--
-    }
-    
-    for idx := 0; idx < len(freq); idx++ {
-        if freq[idx] != 0 {
-            return false
-        }
-    }
-    
-    return true
+		return false
+	}
+
+	var freq [26]int
+
+	for idx := 0; idx < len(s); idx++ {
+		log.Println(s[idx] - 'a')
+		log.Println(t[idx] - 'a')
+
+		freq[s[idx]-'a']++
+		freq[t[idx]-'a']--
+	}
+
+	for idx := 0; idx < len(freq); idx++ {
+		if freq[idx] != 0 {
+			return false
+		}
+	}
+
+	return true
 }
