2ef430620c0e9827dbfacae95d397bf78dd16346
Oleksii Timofieiev <contrbas@gmail.com>
2023-06-15 13:00:53 +0300

    new

--- Algo/main.go
+++ Algo/main.go
@@ -12,6 +12,5 @@ func main() {
 	log.Println(arrayshashing.ReplaceElements([]int{17,18,5,4,6,1}))
 	log.Println(arrayshashing.IsSubsequence("abc", "ahbgdc"))
 	log.Println(arrayshashing.LengthOfLastWord("luffy is still joyboy"))
-
-
+	
 }
